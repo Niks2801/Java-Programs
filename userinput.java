33ad89e0ef2ee4aab08d78dfda342de84ad6fc02
Krishnakant <109541733+Niks2801@users.noreply.github.com>
2024-09-12 00:37:19 +0530

    Update and rename Sept.java to userinput.java

--- userinput.java
+++ userinput.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Sept {
+public class userinput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("please enter some input");
