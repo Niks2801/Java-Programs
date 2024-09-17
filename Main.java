c4c30a949123b5bdfab41e1dd36e953b4f159d87
Krishnakant <109541733+Niks2801@users.noreply.github.com>
2024-09-17 23:02:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String fruit= in.next();
+
+        switch(fruit) {
+            case "Mango":
+                System.out.println("king of fruit");
+                break;
+            case "apple":
+                System.out.println("a sweet red fruit");
+                break;
+            case "orange":
+                System.out.println("round fruit");
+                break;
+
+
+
+        }
+
+
+
+
+    }
+}
