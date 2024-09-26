bead91bf9bfb19a85adc05adccb23e7bed2b623f
Krishnakant <109541733+Niks2801@users.noreply.github.com>
2024-09-26 23:36:14 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int [] ros;
+        ros = new int[5];
+        System.out.println(ros[1]);
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+
+    }
+}
